6010aa0530dc61ecca6ac63b8d9f9b1836b3d53b
Julian Grahsl <julian@newt.localdomain.com>
2013-01-20 11:27:28 +0100

    Added script for automatic compilation for simulation

---
+++ sim.sh
@@ -0,0 +1,17 @@
+set -e
+
+cd asm
+ant -f jopsim.xml
+cd ..
+ant jop-config
+ant java-app
+cd xilinx/atlys
+./mem.sh
+coregen -r -b ipcore/xram.xco  -p ipcore/coregen.cgp
+coregen -r -b ipcore/sysram.xco  -p ipcore/coregen.cgp
+./build
+cd ../..
+
+cd xilinx/atlys/; _flcli --vp=1443:0007 --ivp=1443:0007 -s -x top.xsvf; cd ../..;
+
+minicom
