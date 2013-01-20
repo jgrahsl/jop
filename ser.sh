9b601e8d1adf8a2df6a5a0f4cb73d2f9149d4484
Julian Grahsl <julian@newt.localdomain.com>
2013-01-20 15:03:40 +0100

    reloadedjava app without recompile.

--- ser.sh
+++ ser.sh
@@ -10,11 +10,11 @@ ant java-app
 cd xilinx/atlys
 ./mem.sh
 coregen -r -b ipcore/xram.xco  -p ipcore/coregen.cgp
-coregen -r -b ipcore/sysram.xco  -p ipcore/coregen.cgp
+#coregen -r -b ipcore/sysram.xco  -p ipcore/coregen.cgp
 ./build
 cd ../..
 
 cd xilinx/atlys/; _flcli --vp=1443:0007 --ivp=1443:0007 -s -x top.xsvf; cd ../..;
 
-java download
+ant download
 #Beware of echo
